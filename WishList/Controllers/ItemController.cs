08eaeae58b484573ec94685112913bda6485acfa
Shahrukh Alam <srk.cps12345@gmail.com>
2021-08-23 00:08:31 +0530

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -20,26 +20,29 @@ namespace WishList.Controllers
             var model = _context.Items.ToList();
             return View("Index",model);
         }
+
         [HttpGet]
         public IActionResult Create()
         {
 
             return View("Create");
         }
+
         [HttpPost]
         public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
             _context.SaveChanges();
-            return View("Index");
+            return RedirectToAction("Index");
         }
+
         [HttpPost]
         public IActionResult Delete(int id)
         {
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
-            return View("Index");
+            return RedirectToAction("Index");
         }
     }
 }
