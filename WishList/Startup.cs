973ce93b312f8583029b315e1322466e1b226995
Shahrukh Alam <srk.cps12345@gmail.com>
2021-08-22 22:59:37 +0530

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -24,6 +24,10 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -32,10 +36,7 @@ namespace WishList
             {
                 app.UseExceptionHandler("Home/Error");
             }
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
+            
         }
     }
 }
